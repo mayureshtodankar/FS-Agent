e636e3216b89bd52b3f61a078a8dccd9daceffc8
Mayuresh <87415273+mayureshtodankar@users.noreply.github.com>
2023-01-18 17:43:48 +0530

    Delete GetFSFiles.sh

--- GetFSFiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -L -o /Users/Shared/HexnodeMDM/InstallAgent.sh https://raw.githubusercontent.com/mayureshtodankar/FS-Agent/main/InstallAgent.sh
-./Users/Shared/HexnodeMDM/InstallAgent.sh
-exit 0
