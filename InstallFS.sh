7f519a31ba17f3501d409f7c736d55c4bc7c6b98
Mayuresh <87415273+mayureshtodankar@users.noreply.github.com>
2023-01-18 13:17:07 +0530

    Add files via upload

---
+++ InstallFS.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo rm -rf /Users/Shared/HexnodeMDM/FS-Agent
+sudo git clone https://github.com/mayureshtodankar/FS-Agent.git /Users/Shared/HexnodeMDM/FS-Agent
+cd /Users/Shared/HexnodeMDM/FS-Agent
+sudo /usr/sbin/installer -allowUntrusted -pkg /users/Shared/HexnodeMDM/FS-Agent/FS-Agent.pkg -target /
+exit 0
