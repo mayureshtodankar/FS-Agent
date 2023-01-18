a5bdbe84f870924269095ae00e39ed66fc58fa43
Mayuresh <87415273+mayureshtodankar@users.noreply.github.com>
2023-01-18 17:29:00 +0530

    Add files via upload

---
+++ GetFSFiles.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -L -o /Users/Shared/HexnodeMDM/InstallAgent.sh https://raw.githubusercontent.com/mayureshtodankar/FS-Agent/main/InstallAgent.sh
+./Users/Shared/HexnodeMDM/InstallAgent.sh
+exit 0
